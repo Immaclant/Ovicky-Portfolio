97de17729491f65e6abeee37252db22bf857d4ad
Immaclant <goodnessewaoluwa@gmail.com>
2026-04-05 19:16:35 +0100

    added comment.style
    some stuffs

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,16 +1,21 @@
 function Comment() {
   return (
-    <section comment-page>
-      <div className="comment-header">WHAT OTHERS SAYS</div>
-      <div>
-        {/* ratings */}
-        <h2 className="bold">
-          "He is a very passionate person, who takes all his works seriously as
-          if its his own, kudos to him."
-        </h2>
-        <img src="" alt="" className="comment-img" />
-        <p className="description"></p>
-        <p className="workplace"></p>
+    <section className="comment-page">
+      <div className="comment-card">
+        <div className="comment-header">WHAT OTHERS SAYS</div>
+        <div className="comment-main">
+          <div className="comment-text">
+            {/* ratings */}
+            <h2 className="bold">
+              "He is a very passionate person, who takes all his works seriously as
+              if its his own, kudos to him."
+            </h2>
+            <p className="description"></p>
+            <p className="workplace"></p>
+          </div>
+
+          <img src="" alt="Testimonial" className="comment-img" />
+        </div>
       </div>
     </section>
   );
